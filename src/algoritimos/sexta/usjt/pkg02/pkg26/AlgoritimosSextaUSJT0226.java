9a2f3a20be74c123ccf9261c95c4452df5073966
PK Kyo <caihmrique1@gmail.com>
2026-08-29 00:00:05 -0300

    Exercicio com numeros positivos, negativos e zeros

--- src/algoritimos/sexta/usjt/pkg02/pkg26/AlgoritimosSextaUSJT0226.java
+++ src/algoritimos/sexta/usjt/pkg02/pkg26/AlgoritimosSextaUSJT0226.java
@@ -3,10 +3,43 @@ import java.util.Scanner;
 public class AlgoritimosSextaUSJT0226 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Aula de sexta wow");
-        int num = entrada.nextInt();
-        System.out.println("Digita: ");
-        System.out.println("Numero: "+num);
+        int teste, somaPos = 0 , qntdNeg = 0, qntdPar = 0, qntdImpar = 0, qntdZero = 0;
+        
+        for (teste = 1; teste <=6; teste++)
+        {
+            System.out.println("----- Teste "+teste+" -----"); //deve ter um jeito mais bonitinho
+            System.out.print("Digite um numero: ");
+            int num = entrada.nextInt();
+            
+            if(num > 0)
+            {
+                somaPos = somaPos + num;
+                if(num % 2 == 0)
+                {
+                    qntdPar++;
+                }
+                else{
+                    qntdImpar++;
+                }
+            }
+            else    
+            {
+                if(num < 0)
+                {
+                    qntdNeg++;
+                }
+                else
+                {
+                    qntdZero++;
+                }
+            }
+        }
+        System.out.println("Numeros positivos pares: "+ qntdPar);
+        System.out.println("Numeros positivos impares: "+ qntdImpar);
+        System.out.println("Numeros negativos: "+ qntdNeg);
+        System.out.println("Quantidade de zeros: "+ qntdZero);
+        System.out.println("Soma dos numeros positivos: "+   somaPos);
+                
     }
     
 }
